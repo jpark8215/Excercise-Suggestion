9753ae780aa0667dfaafd70ae2c553442fd80940
jpark8215 <50881191+jpark8215@users.noreply.github.com>
2023-02-12 10:59:55 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from basic import exercise_suggestions
+
+
+def main():
+    print("Welcome to Exercise Suggestions Generator")
+    height = float(input("Please enter your height in centimeters: "))
+    weight = float(input("Please enter your weight in kilograms: "))
+    time = float(input("Please enter the amount of time you have for exercise in minutes: "))
+    exercise_suggestions(height, weight, time)
+
+if __name__ == "__main__":
+    main()
+
+
